96fa297a3484562bae2ff72cb484cf76bdfa870d
felipemaya <pipe.040691@gmail.com>
2023-01-18 00:55:18 -0500

    con catch podemos capturar los errores que pueden aparecer durante la peticion ej. error de conexion, problemas con la url entre otros

--- 36-fetch.js
+++ 36-fetch.js
@@ -42,14 +42,14 @@ function listarUsuario(usuario){
 
 function getInfo(){
     let profesor = {
-        nombre: 'felipe',
+        nombre: 'Felipe',
         apellido: 'Maya',
         url: "hola mundo"
     };
     return new Promise((resolve, reject)=>{ // creando una promesa se inicializan dos funciones "reject"  y "resolve" para rechazar si hay algun error o para devolver el dato solicitado
         let profesor_string = '';
         setTimeout(function(){ // con setTimeout podemos observar como la promesa mientras se termina de ejecutar se esperan las siguientes
-            profesor_string = JSON.stringify(profesor); // convertimos la varible en un objeto JSON
+            profesor_string = JSON.stringify(profesor); // convertimos el array en un string 
             
             if(typeof profesor_string != 'string' || profesor_string == '') return reject('error') // validamos los datos si son del tipo correcto y que no esté vacío
 
@@ -68,7 +68,8 @@ getUsers()
 })
 .then(data =>{
     console.log(data);
-    pProfe.innerHTML = data;
+    let profesor_json = JSON.parse(data); // convertimos el string en un objeto JSON
+    pProfe.innerHTML = profesor_json.nombre+" "+profesor_json.apellido;
     divProfe.querySelector('.loading').style.display = 'none';
     divProfe.appendChild(pProfe);
     return getUser(2);
@@ -79,3 +80,6 @@ getUsers()
     oneUser.appendChild(listarUsuario(user));
     oneUser.querySelector('.loading').style.display = 'none';
 })
+.catch(error => { // con catch po demo capturar lo errores que pueden aparecer por alguna circunstancia 
+    console.error(error);
+});
